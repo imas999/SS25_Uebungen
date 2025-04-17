7ba0d01b9d392b812b06235198e0b5438b125847
Sami <53356202+imas999@users.noreply.github.com>
2025-04-17 17:47:43 +0200

    Grundlegende Autovermietung

--- Autovermietung.cpp
+++ Autovermietung.cpp
@@ -1,20 +1,160 @@
-// Autovermietung.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Fahrt.cpp"
+#include "Mietwagen.cpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+class Autovermietung {
+private:
+    vector<Mietwagen> fahrzeuge;
+
+public:
+    Autovermietung() : fahrzeuge(vector<Mietwagen>()) {}
+
+    // Getter und Setter
+    vector<Mietwagen> getFahrzeuge() { return fahrzeuge; }
+    void setFahrzeuge(vector<Mietwagen> neueFahrzeuge) { fahrzeuge = neueFahrzeuge; }
+
+    vector<Fahrt> getFahrtenbuch() { return fahrtenbuch; }
+
+    void setFahrtenbuch(vector<Fahrt> neueFahrten) { fahrtenbuch = neueFahrten; }
+
+    //Destruktor für Mietwagen, welche
+    ~Autovermietung() {
+            fahrzeuge.clear();
+            cout << "Vielen Dank!" << endl;
+    }
+
+    //Fügt ein Fahrzeug zur Fahrzeugliste hinzu
+    void addFahrzeug(Mietwagen wagen) {
+        fahrzeuge.push_back(wagen);
+        cout << "Fahrzeug hinzugefügt: " << wagen.getMarke() << endl;
+    }
+
+    //Eröffnet einen Konsolendialog zum Verwalten der Autovermietung
+    void dialog() {
+        int auswahl = 1;
+        while (auswahl != 0) {
+            cout << "1. Fahrzeug mieten" << endl;
+            cout << "2. Fahrt anzeigen" << endl;
+            cout << "3. Alle Fahrten anzeigen" << endl;
+            cout << "0. Programm Beenden" << endl;
+            cin >> auswahl;
+
+            switch (auswahl) {
+                case 1: // Fahrzeug mieten
+                {
+                    int nummer, abholdatum, abgabedatum;
+                    string kundenname, kennzeichen;
+    
+                    cout << "Bitte geben Sie das KFZ-Kennzeichen ein: ";
+                    cin >> kennzeichen;
+
+                    Mietwagen* w = findeFahrzeug(kennzeichen);
+
+                    cout << "Bitte geben Sie die Fahrtnummer ein: ";
+                    cin >> nummer;
+
+                    if (w->nummerPruefen(nummer)) {
+                        cout << "Fahrtnummer existiert bereits." << endl;
+                        break;
+                    } 
+
+                    cout << "Bitte geben Sie den Namen des Kunden ein: ";
+                    cin.ignore();
+                    getline(cin, kundenname);
+
+                    cout << "Bitte geben Sie das Abholdatum (im Format JJJJMMTT) ein: ";
+                    cin >> abholdatum;
+
+                    cout << "Bitte geben Sie das Abgabedatum (im Format JJJJMMTT) ein: ";
+                    cin >> abgabedatum;
+
+                    Fahrt f(nummer, kundenname, abholdatum, abgabedatum); 
+                
+                    w->anmieten(f); 
+                    break;
+                }
+                case 2: // Fahrt anzeigen
+                {
+                    int fahrtennummer;
+                    cout << "Buchungsnummer? ";
+                    cin >> fahrtennummer;
+
+                    bool gefunden = false;
+                    for (Mietwagen wagen : fahrzeuge) {
+                        for (Fahrt f : wagen.getFahrtenbuch()) {
+                            if (f.getNummer() == fahrtennummer) {
+                                gefunden = true;
+                                //f.anzeigen();
+                                Fahrt fahrt(f.getNummer(), f.getKunde(), f.getAbholdatum(), f.getAbgabedatum());
+                                fahrt.anzeigen();
+                                break;
+                            }
+                        }
+                    }
+                    if (gefunden = 0) cout << "Fahrt existiert nicht." << endl;
+                    break;
+                }
+                case 3: // Alle Fahrten anzeigen
+                {  
+                    for(Mietwagen w : fahrzeuge) {
+                        w.alleFahrtenAnzeigen();
+                        cout << "--------------------------" << endl;
+                    }
+                    break;
+                }
+                case 0: 
+                {
+                    cout << "Auf Wiedersehen!" << endl;
+                    break;
+                }
+                default: 
+                    cout << "Ungültige Auswahl, bitte versuchen Sie es erneut." << endl;
+            }
+        }
+    }
+
+    Mietwagen* findeFahrzeug(string kennzeichen) {
+        for (Mietwagen& w : fahrzeuge) {
+            if (w.getKennzeichen() == kennzeichen) {return &w; }
+        }
+        throw runtime_error("Fahrzeug mit dem angegebenen Kennzeichen wurde nicht gefunden.");
+    }
+
+};
+
+int main() {
+    string marke, kennzeichen;
+    int sitze;
+    cout << "Neues Fahrzeug: " << endl;
+    cout << "Automarke? ";
+    cin >> marke;
+    cout << "KFZ-Kennzeichen? ";
+    cin >> kennzeichen;
+    cout << "Sitze? ";
+    cin >> sitze;
+    
+    Mietwagen wagen(marke, kennzeichen, sitze);
+    Mietwagen wagen2("Porsche", "KA-AS-1012", 2);
+    Mietwagen wagen3("Opel", "KA-AS-1013", 4);
+
+    Fahrt testfahrt(1, "Nikola,Tesla", 20251001, 20251002);
+    Fahrt testfahrt2(2, "Charles,Darwin", 20250101, 20250212);
+
+    wagen.anmieten(testfahrt);
+
+    Autovermietung KarlsruheCars;
+
+    KarlsruheCars.addFahrzeug(wagen); 
+    KarlsruheCars.addFahrzeug(wagen2);
+
+    Mietwagen* w = KarlsruheCars.findeFahrzeug("KA-AS-1012");
+    w->anmieten(testfahrt2);
+  
+    KarlsruheCars.dialog(); 
+
+    return 0;
 }
-
-// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
